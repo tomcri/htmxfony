9e68fa01b0a0d859d465de04ef571a0ba21a3532
Cristian Toma <cristian.toma@vivre.eu>
2023-11-08 18:47:21 +0200

    feat: enforce tests - HtmxRequest available in EventSubscribers

--- tests/HtmxKernelTraitTest.php
+++ tests/HtmxKernelTraitTest.php
@@ -11,13 +11,16 @@ use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class HtmxKernelTraitTest extends TestCase
 {
@@ -25,7 +28,7 @@ class HtmxKernelTraitTest extends TestCase
     public function testHtmxKernelTrait(): void
     {
         $container = $this->createMock(ContainerInterface::class);
-        $container->method('get')->willReturnCallback(function ($id) {
+        $container->method('get')->willReturnCallback(callback: function ($id) {
             if ($id === 'http_kernel') {
                 $controllerResolver = $this->createMock(ControllerResolverInterface::class);
                 $controllerResolver->method('getController')->willReturn(function (Request $request) {
@@ -34,8 +37,38 @@ class HtmxKernelTraitTest extends TestCase
                     return new Response('', Response::HTTP_OK);
                 });
 
+                $assertRequest = function ($request) {
+                    $this->assertInstanceOf(HtmxRequest::class, $request);
+                };
+                $eventDispatcher = new EventDispatcher();
+                $eventDispatcher->addSubscriber(new class($assertRequest) implements EventSubscriberInterface {
+
+                    /** @var callable */
+                    private $assertRequest;
+
+                    public function __construct(callable $assertRequest)
+                    {
+                        $this->assertRequest = $assertRequest;
+                    }
+
+                    public static function getSubscribedEvents()
+                    {
+                        return [
+                            KernelEvents::REQUEST => [
+                                ['testRequest', 90],
+                            ],
+                        ];
+                    }
+
+                    public function testRequest(RequestEvent $event): void
+                    {
+                        ($this->assertRequest)($event->getRequest());
+                    }
+
+                });
+
                 return new HttpKernel(
-                    new EventDispatcher(),
+                    $eventDispatcher,
                     $controllerResolver,
                     new RequestStack(),
                     new ArgumentResolver()
